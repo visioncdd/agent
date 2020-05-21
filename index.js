e9528bb86018ccf93a4f22bb2ce0be77e438685c
Luis Garate <visioncdd@gmail.com>
2020-05-21 15:11:20 -0400

    fix

--- index.js
+++ index.js
@@ -485,6 +485,13 @@ client.connect(function(err) {
 
 			break;
 
+			case 'question_type_payment':
+
+				// if(!data.length && solicitud.last_state == 'disponibilidad_multiple')
+					message += "En efectivo o transferencia"
+
+			break;
+
 			case 'pedido':
 
 				if(!data.length){
