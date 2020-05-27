380f61e4ca5400861394915960acd5a3d6bb7da0
Luis Garate <visioncdd@gmail.com>
2020-05-27 16:23:14 -0400

    fix

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ client.connect(function(err) {
 		// var empresa = await getCompany(req.params.id)
 		console.log(req.body,req.params)
 		delete req.body._id
-		db1.collection('empresas').updateOne({ _id: ObjectId(req.params.id) }, req.body, {upsert: true}).then(doc => {
+		db1.collection('empresas').updateOne({ _id: ObjectId(req.params.id) }, { $set: req.body }, {upsert: true}).then(doc => {
 			console.log(doc)
 			res.json(doc)
 		})
