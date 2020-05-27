71d6a07b890dd3b0e2848768a509d2b6f62cebca
Luis Garate <visioncdd@gmail.com>
2020-05-27 19:22:15 -0400

    fix

--- index.js
+++ index.js
@@ -280,6 +280,7 @@ client.connect(function(err) {
 				}], function(err, data){
 					
 					data.limit(1).toArray(function(err,list){
+						console.log(list)
 						resolve(list.length && list[0].score >= 0.80 ? list[0].action : null)
 					})
 				})
